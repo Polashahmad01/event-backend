bfe64c112c953e970b0c50b81d72de159fbc1f81
Polashahmad01 <dev.polashahmad@gmail.com>
2023-03-06 13:14:20 +0600

    add initial search endpoint

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ require('dotenv').config()
 
 const eventRoute = require("./api/event")
 const tagsRoute = require("./api/tags")
+const searchRoute = require("./api/search")
 
 const PORT = process.env.PORT || 5001
 
@@ -16,6 +17,7 @@ app.get("/", (req, res) => {
   res.status(200).json({ success: true, data: "Server is running" })
 })
 
+app.use("/api", searchRoute)
 app.use("/api", eventRoute)
 app.use("/api", tagsRoute)
 
