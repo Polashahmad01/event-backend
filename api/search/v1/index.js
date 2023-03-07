5470ed060e224e89371ffbd49f2c98d3b70a684d
Polashahmad01 <dev.polashahmad@gmail.com>
2023-03-07 21:53:50 +0600

    add type status tag and search features

--- api/search/v1/index.js
+++ api/search/v1/index.js
@@ -5,36 +5,62 @@ const mongo = require("../../../services/mongo-crud")
 
 const searchContents = async (req, res) => {
   const { db, client } = await mongoConnect()
+  const requestBody = Object.keys(req.body).length === 0
+
+  if(!requestBody) {
+    const { eventTypes, statuses, tags, query } = req.body  
+    const eventType = Object.keys(eventTypes).length === 0
+    const status = Object.keys(statuses).length === 0
+    const tag = Object.keys(tags).length === 0
+    const searchTerm = Object.keys(query).length === 0
+
+    // If all fields are blank
+    if(eventType && status && tag && searchTerm) {
+      const events = await mongo.search(db, "events")
+      return res.status(200).json({ success: true, count: events.length, events })
+    }
   
-  if(req.body.eventTypes) {
-    const queryObj = {
-      "$or": []
-    }
-    const hasGoogleDocEvents = req.body.eventTypes.includes("google_doc")
-    const hasLinkEvents = req.body.eventTypes.includes("link")
-    const hasPdfEvents = req.body.eventTypes.includes("pdf")
-    const hasYouTubeEvents = req.body.eventTypes.includes("youtube")
+    // filtered response
+    if(!eventType || !status || !tag || !searchTerm) {
+      let query = {}
 
-    if(hasGoogleDocEvents) {
-      queryObj.$or.push({ eventType: "google_doc" })
-    }
+      if(!eventType) {
+        query = {
+          ...query,
+          eventType: { $in: req.body.eventTypes }
+        }
+      }
 
-    if(hasLinkEvents) {
-      queryObj.$or.push({ eventType: "link" })
-    }
+      if(!status) {
+        query = {
+          ...query,
+          status: { $in: req.body.statuses}
+        }
+      }
 
-    if(hasPdfEvents) {
-      queryObj.$or.push({ eventType: "pdf" })
-    }
+      if(!tag) {
+        query = {
+          ...query,
+          "tags._id": { $in: req.body.tags }
+        }
+      }
 
-    if(hasYouTubeEvents) {
-      queryObj.$or.push({ eventType: "youtube" })
-    }
+      if(!searchTerm) {
+        query = {
+          ...query,
+          $or: [
+            { title: { $regex: `(?i)${req.body.query}(?-i)` }},
+            { description: { $regex: `(?i)${req.body.query}(?-i)` }}
+          ]
+        }
+      }
 
-    const events = await mongo.search(db, "events", queryObj)
-    return res.status(200).json({ success: true, count: events.length, events })    
+      const events = await mongo.search(db, "events", query)
+      return res.status(200).json({ success: true, count: events.length, events })
+    }
   }
 
+  // Default response
   try {
     const events = await mongo.search(db, "events")
     res.status(200).json({ success: true, count: events.length, events })
